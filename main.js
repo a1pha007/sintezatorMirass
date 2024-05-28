b892e8f5266f842988377866e5da8292857fe40a
alpha_dev <mirasaltinbekovn@gmail.com>
2024-05-28 16:15:36 +0500

    ishlar bitirildi

---
+++ main.js
@@ -0,0 +1,85 @@
+document.addEventListener("keyup", (e) => {
+    let audio;
+    if (e.key === "1") {
+        document.body.style.background = "red";
+        audio = new Audio("back1.mp3");
+        audio.play();
+    } else if (e.key === "2") {
+        document.body.style.background = "green";
+        audio = new Audio("back1edit.mp3");
+        audio.play();
+    } else if (e.key === "3") {
+        document.body.style.background = "blue";
+        audio = new Audio("back2.mp3");
+        audio.play();
+    } else if (e.key === "4") {
+        document.body.style.background = "yellow";
+        audio = new Audio("back3.mp3");
+        audio.play();
+    } else if (e.key === "5") {
+        document.body.style.background = "skyblue";
+        audio = new Audio("dudustart.mp3");
+        audio.play();
+    } else if (e.key === "6") {
+        document.body.style.background = "dodgerblue";
+        audio = new Audio("scary.mp3");
+        audio.play();
+    } else if (e.key === "7") {
+        document.body.style.background = "orange";
+        audio = new Audio("hahaha.mp3");
+        audio.play();
+    } else if (e.key === "0") {
+        document.body.style.background = "white";
+        audio = new Audio("1.mp3");
+        audio.play();
+    }
+});
+document.addEventListener("keyup", (e) => {
+    let audio;
+    if (e.key === "a") {
+        document.body.style.background = "red";
+        audio = new Audio("doira.mp3");
+        audio.play();
+    } else if (e.key === "s") {
+        document.body.style.background = "green";
+        audio = new Audio("doira2.mp3");
+        audio.play();
+    } else if (e.key === "d") {
+        document.body.style.background = "blue";
+        audio = new Audio("doira3.mp3");
+        audio.play();
+    } else if (e.key === "f") {
+        document.body.style.background = "yellow";
+        audio = new Audio("fuuuu.mp3");
+        audio.play();
+    } else if (e.key === "j") {
+        document.body.style.background = "skyblue";
+        audio = new Audio("mm.mp3");
+        audio.play();
+    } else if (e.key === "k") {
+        document.body.style.background = "dodgerblue";
+        audio = new Audio("music1.mp3");
+        audio.play();
+    } else if (e.key === "l") {
+        document.body.style.background = "orange";
+        audio = new Audio("nn.mp3");
+        audio.play();
+    } else if (e.key === ";") {
+        document.body.style.background = "white";
+        audio = new Audio("taktak.mp3");
+        audio.play();
+    } else if (e.key === "g") {
+        document.body.style.background = "dodgerblue";
+        audio = new Audio("tiktuktuk.mp3");
+        audio.play();
+    } else if (e.key === "h") {
+        document.body.style.background = "orange";
+        audio = new Audio("tvutvut.mp3");
+        audio.play();
+    } else if (e.key === "'") {
+        document.body.style.background = "white";
+        audio = new Audio("taktak.mp3");
+        audio.play();
+    }
+});
+
